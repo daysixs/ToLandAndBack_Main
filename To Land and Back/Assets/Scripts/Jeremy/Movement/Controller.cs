3ebcd8e6986fe211fde89b903dfedc0381c54ff2
pinkprosciutto <74938181+pinkprosciutto@users.noreply.github.com>
2021-09-28 19:50:37 +0800

    Added Ladder mechanics

--- To Land and Back/Assets/Scripts/Jeremy/Movement/Controller.cs
+++ To Land and Back/Assets/Scripts/Jeremy/Movement/Controller.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 [RequireComponent (typeof (BoxCollider2D))] //automatically adds the "BoxCollider2D" component
 public class Controller : Raycast
 {
-    public collisionInfo collisions;
+    public CollisionInfo collisions;
     float maxClimbAngle = 80f;
     float maxDescendAngle = 75f;
 
     public LayerMask collisionMask;
+    public LayerMask ladderLayer;
 
     public override void Start()
     {
@@ -18,24 +19,21 @@ public class Controller : Raycast
     }
 
     //movement of the character
-    public void Move(Vector3 velocity)
+    public void Movement(Vector3 velocity)
     {
         UpdateRaycastOrigin();
-        collisions.reset();
+        collisions.Reset();
         collisions.oldVelocity = velocity;
 
         if (velocity.x != 0)
-        {
             collisions.faceDirection = (int)Mathf.Sign(velocity.x);
-        }
 
         //call descendSlope method when the character is walking down a slope
         if (velocity.y < 0)
-        {
-            descendSlope(ref velocity);
-        }
+            DescendSlope(ref velocity);
 
         HorizontalCollision(ref velocity);
+        LadderCollision(ref velocity);
 
         if (velocity.y != 0)
         {
@@ -45,66 +43,10 @@ public class Controller : Raycast
         
     }
 
-    void HorizontalCollision(ref Vector3 velocity)
-    {
-        float directionX = collisions.faceDirection;
-        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
-
-        if (Mathf.Abs(velocity.x) < skinWidth)
-        {
-            rayLength = 2 * skinWidth;
-        }
-
-        for (int i = 0; i < horizontalRayCount; i++)
-        {
-            Vector2 rayOrigin = (directionX == -1) ? RaycastOrigin.bottomLeft : RaycastOrigin.bottomRight; //checks if the character is moving left or right
-            rayOrigin += Vector2.up * (horizontalRaySpacing * i); //calculating ray position when the character has moved
-            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask); //collision detection below the player
-
-            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.green); //draw raycast vertically
-
-            if (hit)
-            {
-                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
-
-                if (i == 0 && slopeAngle <= maxClimbAngle)
-                {
-                    if (collisions.descendSlope)
-                    {
-                        collisions.descendSlope = false;
-                        velocity = collisions.oldVelocity;
-                    }
-
-                    //Allows the character to walk on the slope more precisely (ie doesn't hover above the slope)
-                    float distanceToStartSlope = 0f;
-                    if (slopeAngle != collisions.oldSlopeAngle)
-                    {
-                        distanceToStartSlope = hit.distance - skinWidth;
-                        velocity.x -= distanceToStartSlope * directionX;
-                    }
-                    climbSlope(ref velocity, slopeAngle); //allows the character to walk on slopes
-                    velocity.x += distanceToStartSlope * directionX;
-                }
-
-                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle)
-                {
-                    velocity.x = (hit.distance - skinWidth) * directionX; //stops the character from going through walls
-                    rayLength = hit.distance; //prevents the character from falling when only half of the hitbox is in the sky
-
-                    if (collisions.climbingSlope)
-                    {
-                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
-
-                    }
-                    collisions.left = directionX == -1; //if moving left and collided with left wall, collisions is true
-                    collisions.right = directionX == 1;
-                }
-            }
-        }
-    }
+   
 
     //mathematical calculations to allows smoother climbing on the slopes
-    void climbSlope(ref Vector3 velocity, float slopeAngle)
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle)
     {
         float moveDistance = Mathf.Abs(velocity.x);
         float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
@@ -114,7 +56,7 @@ public class Controller : Raycast
         {
             velocity.y = climbVelocityY;
             velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x); //prevents the character from rapidly shaking when colliding with walls on slopes
-            collisions.below = true;
+            collisions.bottom = true;
             collisions.climbingSlope = true;
             collisions.slopeAngle = slopeAngle;
         }
@@ -122,7 +64,7 @@ public class Controller : Raycast
     }
 
     //allows the character to move down a slope smoothly
-    void descendSlope(ref Vector3 velocity)
+    void DescendSlope(ref Vector3 velocity)
     {
         float directionX = Mathf.Sign(velocity.x);
         Vector2 rayOrigin = (directionX == -1) ? RaycastOrigin.bottomRight : RaycastOrigin.bottomLeft;
@@ -145,9 +87,67 @@ public class Controller : Raycast
 
                         collisions.slopeAngle = slopeAngle;
                         collisions.descendSlope = true;
-                        collisions.below = true;
+                        collisions.bottom = true;
                         
                     }
+                }
+            }
+        }
+    }
+
+    void HorizontalCollision(ref Vector3 velocity)
+    {
+        float directionX = collisions.faceDirection;
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        if (Mathf.Abs(velocity.x) < skinWidth)
+        {
+            rayLength = 2 * skinWidth;
+        }
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? RaycastOrigin.bottomLeft : RaycastOrigin.bottomRight; //checks if the character is moving left or right
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i); //calculating ray position when the character has moved
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask); //collision detection bottom the player
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.green); //draw raycast vertically
+
+            if (hit)
+            {
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+                if (i == 0 && slopeAngle <= maxClimbAngle)
+                {
+                    if (collisions.descendSlope)
+                    {
+                        collisions.descendSlope = false;
+                        velocity = collisions.oldVelocity;
+                    }
+
+                    //Allows the character to walk on the slope more precisely (ie doesn't hover top the slope)
+                    float distanceToStartSlope = 0f;
+                    if (slopeAngle != collisions.oldSlopeAngle)
+                    {
+                        distanceToStartSlope = hit.distance - skinWidth;
+                        velocity.x -= distanceToStartSlope * directionX;
+                    }
+                    ClimbSlope(ref velocity, slopeAngle); //allows the character to walk on slopes
+                    velocity.x += distanceToStartSlope * directionX;
+                }
+
+                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle)
+                {
+                    velocity.x = (hit.distance - skinWidth) * directionX; //stops the character from going through walls
+                    rayLength = hit.distance; //prevents the character from falling when only half of the hitbox is in the sky
+
+                    if (collisions.climbingSlope)
+                    {
+                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+
+                    }
+                    collisions.left = directionX == -1; //if moving left and collided with left wall, collisions is true
+                    collisions.right = directionX == 1;
                 }
             }
         }
@@ -162,9 +162,9 @@ public class Controller : Raycast
         {
             Vector2 rayOrigin = (directionY == -1) ? RaycastOrigin.bottomLeft : RaycastOrigin.topLeft; //if falling down, start from bottom left, otherwise top left
             rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x); //calculating ray position when the character has moved
-            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask); //collision detection below the player
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask); //collision detection bottom the player
 
-            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.green); //draw raycast below the character
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.green); //draw raycast bottom the character
 
             if (hit)
             {
@@ -175,8 +175,8 @@ public class Controller : Raycast
                 {
                     velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x); //prevents the character from rapidly shaking when colliding with walls on slopes
                 }
-                collisions.above = directionY == 1;
-                collisions.below = directionY == -1;
+                collisions.top = directionY == 1;
+                collisions.bottom = directionY == -1;
             }
         }
 
@@ -199,21 +199,41 @@ public class Controller : Raycast
         }
     }
 
-
-    public struct collisionInfo
+    void LadderCollision(ref Vector3 velocity)
     {
-        public bool above, below, left, right;
+        //RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, ladderLayer);
+
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? RaycastOrigin.bottomLeft : RaycastOrigin.topLeft; //if falling down, start from bottom left, otherwise top left
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x); //calculating ray position when the character has moved
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, ladderLayer); //collision detection bottom the player
+
+            if (hit)
+            {
+                collisions.climbingLadder = true;
+            }
+        }
+    }
+    public struct CollisionInfo
+    {
+        public bool top, bottom, left, right;
         public bool climbingSlope, descendSlope;
+        public bool climbingLadder;
         public float slopeAngle, oldSlopeAngle;
         public Vector3 oldVelocity;
         public int faceDirection;
 
-        public void reset()
+        public void Reset()
         {
-            above = below = false;
+            top = bottom = false;
             left = right = false;
             climbingSlope = false;
             descendSlope = false;
+            climbingLadder = false;
             oldSlopeAngle = slopeAngle;
             slopeAngle = 0;
         }
