a0f4e412356e74aef84ff185c0a73ce01ec8559d
pinkprosciutto <74938181+pinkprosciutto@users.noreply.github.com>
2021-09-28 13:57:33 +0800

    Update to Movement Mechanics

--- To Land and Back/Assets/Scripts/Jeremy/Movement/Player.cs
+++ To Land and Back/Assets/Scripts/Jeremy/Movement/Player.cs
@@ -1,19 +1,57 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent (typeof (Controller))] //automatically adds the "Controller" script 
 public class Player : MonoBehaviour
 {
+    Vector3 velocity;
+    float gravity;
+
+    [Header("Movement stats")]
+    public float speed = 6f;
+    float accelerationTimeInAir = .2f;
+    float accelerationTimeInGround = .1f;
+
+    [Header("Jump")]
+    public float jumpVelocity;
+    public float jumpHeight = 4f;
+    public float timeToJump = .4f;
+    public bool doubleJump;
+
+    float velocityXSmoothing;
+
     Controller controller;
 
     void Start()
     {
         controller = GetComponent<Controller>();
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJump, 2); 
+        jumpVelocity = Mathf.Abs(gravity) * timeToJump;
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        controller.Move(velocity * Time.deltaTime);
+
+        //prevents gravity from building up
+        if (controller.collisions.above || controller.collisions.below)
+            velocity.y = 0;
+    }
+
     void Update()
     {
-        
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //Storing input key
+    
+        float velocityX = input.x * speed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, velocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeInGround : accelerationTimeInAir); //vertical movement, slow down smoothly when stopped moving
+        velocity.y += gravity * Time.deltaTime; //gravity
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            if (controller.collisions.below)
+                velocity.y = jumpVelocity;
+        }
     }
 }
