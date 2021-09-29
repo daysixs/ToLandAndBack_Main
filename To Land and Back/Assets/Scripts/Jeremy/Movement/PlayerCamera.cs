12fdd5cec9884364da78fe7b922d24c4fd97f01f
pinkprosciutto <74938181+pinkprosciutto@users.noreply.github.com>
2021-09-29 15:01:43 +0800

    Update

--- To Land and Back/Assets/Scripts/Jeremy/Movement/PlayerCamera.cs
+++ To Land and Back/Assets/Scripts/Jeremy/Movement/PlayerCamera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class PlayerCamera : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
